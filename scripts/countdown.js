fa09b5c6a821e0e5e5f328ad9ed2774012a4d8ba
benjamindobbs <benjamindobbs@gmail.com>
2026-08-13 08:55:27 -0400

    fixed countdown target date and plurarlization of "Day(s)" in countdown

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -1,15 +1,17 @@
 
-const targetDate = new Date("Aug 14, 2026 23:59:59").getTime();
+const targetDate = new Date("Aug 15, 2026 00:00:00").getTime();
 
 function updateCountdown() {
     const now = new Date().getTime();
     const distance = targetDate - now;
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    document.getElementById("days").innerHTML = days;
     if (distance < 0) {
         clearInterval(timer);
         document.getElementById("countdown").innerHTML = "TODAY!";
+        return;
     }
+    const days = Math.ceil(distance / (1000 * 60 * 60 * 24));
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("days-label").innerHTML = days === 1 ? "Day to Go" : "Days to Go";
 }
 
 updateCountdown();
